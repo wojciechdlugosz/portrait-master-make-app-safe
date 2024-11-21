98dcf2fca58ecb25fadbfd77f78e9df477969503
Wojciech Dlugosz <webmaster@styronet.pl>
2024-11-21 15:56:21 +0100

    Fix post endpoint to check file extenstion

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -11,9 +11,14 @@ exports.add = async (req, res) => {
     if(title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      const fileExt = fileName.split('.').slice(-1)[0];
+      if (fileExt === 'gif' || fileExt === 'jpg' || fileExt === 'png') {
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);      
+      } else {
+        throw new Error('Wrong file ext!');
+      }
 
     } else {
       throw new Error('Wrong input!');
